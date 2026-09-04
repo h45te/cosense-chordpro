e81ed45288a1a833430bc1dab2372de8986ee5f8
はすて <104975590+h45te@users.noreply.github.com>
2026-09-04 12:51:37 +0900

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,5 +1,5 @@
 type MetaToken = { role: 'meta'; key: string; value?: string; };
-type Chunk = { chord: string; lyric: string; };
+type Chunk = { chord: string | undefined; lyric: string; };
 type Measure = Chunk[]
 type MeasuresToken = { role: 'measures'; measures: Measure[]; };
 type Token = MetaToken | MeasuresToken;
@@ -62,19 +62,20 @@ class Chordpro {
 	static readonly #measureRegExp = /^\|?(.*?)\|?$/;
 	static readonly #chunkRegExp = /(?!$)(?:\[([^\]]*)\])?([^\[]*)/gm;
 	static readonly #CSS_RULES = [
-		'.mono { font-family: monospace; }',
-		'.measures-container { display: flex; flex-wrap: balance; margin-top: 0.5em; margin-bottom: 0.5em; line-height: 1.2; font-size: 15px; }',
+		'.content { line-height: 1.2; }',
+		'.measures-container { display: flex; flex-wrap: balance; margin-top: 0.5em; margin-bottom: 0.5em; }',
 		'.measures-container > span { display: flex; flex-wrap: balance; }',
 		'.measures-container > span > span { padding-right: 0.5em; }',
-		'.sidemenu-container { position: absolute; right: 0px; }', 
-		'form > * { padding: 5px; }'
+		'.chord { letter-spacing: 0.05em; }',
+		'@media screen and (width >= 600px) { .sidemenu-container { position: absolute; right: 0px; } }', 
+		'form > * { margin: 5px; }'
 	];
 	static readonly #CONTENT_CONTAINER_ID = 'chordpro-content-container';
 	static readonly #SIDEMENU_CONTAINER_ID = 'chordpro-sidemenu-container';
 
 	static #sourceRoot: Element;
 	static #chordproLines: string[];
-	static #chordproTokens: Token[];
+	static #contentElement: HTMLElement;
 
 	static #readElement(): boolean {
 		const codeElements = [...document.getElementsByClassName('code-block')];
@@ -102,12 +103,12 @@ class Chordpro {
 		const measureStringAry = measureMatch[1].split('|');
 		const measures = measureStringAry.map(
 			e => [...e.matchAll(Chordpro.#chunkRegExp)].map(
-				e_ => {return {chord: e_[1], lyric: e_[2]}}
+				e_ => {return {chord: e_[1] as string | undefined, lyric: e_[2]}}
 			)
 		);
 		return measures;
 	}
-	static #parseLines(): void {
+	static #parseLines(): Token[] {
 		const tokens: Token[] = [];
 		for (const e of Chordpro.#chordproLines) {
 			const metaMatch = e.match(Chordpro.#metaRegExp);
@@ -124,10 +125,10 @@ class Chordpro {
 				tokens.push(token);
 			}
 		}
-		Chordpro.#chordproTokens = tokens;
+		return tokens;
 	}
 
-	static #applyStyle() {
+	static #applyStyle(): void {
 		const sheet = new CSSStyleSheet();
 		for (const e of Chordpro.#CSS_RULES) {sheet.insertRule(e)};
 		document.adoptedStyleSheets = [
@@ -136,55 +137,73 @@ class Chordpro {
 		];
 	}
 
-	static #appendMeta({key, value}: MetaToken, parent: Element) {
+	static #createMetaElement({key, value}: MetaToken): HTMLElement {
 		switch (key) {
 			default:
 				const containerElement = document.createElement('div');
 				const contentElement = document.createElement('span');
 				contentElement.textContent = `{${key}:${value}}`;
 				containerElement.appendChild(contentElement);
-				parent.appendChild(containerElement);
-				break;
+				return containerElement;
 		}
 	}
-	static #appendMeasures({measures}: MeasuresToken, parent: Element) {
+	static #createChunkElement (chunk: Chunk): HTMLElement {
+		const chunkElement = document.createElement('span');
+		const chordElement = document.createElement('div');
+		const lyricElement = document.createElement('div');
+
+		chordElement.classList.add('chord')
+		chordElement.textContent = `⁣${chunk.chord ?? ''}`;
+		lyricElement.textContent = `⁣${chunk.lyric}`;
+
+		chunkElement.appendChild(chordElement);
+		chunkElement.appendChild(lyricElement);
+
+		return chunkElement;
+	}
+	static #createMeasureElement(measure: Measure): HTMLElement {
+		const measureElement = document.createElement('span');
+
+		const measureDivider: Chunk = {chord: '|', lyric: '|'};
+		for (const e of [measureDivider, ...measure]) {
+			const c = Chordpro.#createChunkElement(e);
+			measureElement.appendChild(c);
+		}
+
+		return measureElement;
+	}
+	static #createMeasuresElement({measures}: MeasuresToken): HTMLElement {
 		const containerElement = document.createElement('div');
 		containerElement.classList.add('measures-container');
-		for (const e of measures) {
-			const measureStart: Chunk = {chord: '|', lyric: '|'};
-			const measureEnd: Chunk = {chord: '|', lyric: '|'};
 
-			const measureElement = document.createElement('span');
-			for (const e_ of [measureStart, ...e, measureEnd]) {
-				const chunkElement = document.createElement('span');
-				const chordElement = document.createElement('div');
-				const lyricElement = document.createElement('div');
-				chordElement.textContent = `⁣${e_.chord ?? ''}`;
-				lyricElement.textContent = `⁣${e_.lyric}`;
-				chunkElement.appendChild(chordElement);
-				chunkElement.appendChild(lyricElement);
-				measureElement.appendChild(chunkElement);
-			}
-			containerElement.appendChild(measureElement);
+		const containerDivider: Measure = [];
+		for (const e of [...measures, containerDivider]) {
+			const m = Chordpro.#createMeasureElement(e);
+			containerElement.appendChild(m);
 		}
-		parent.appendChild(containerElement);
+
+		return containerElement;
 	}
-	static #appendContent() {
+	static #createTokenElement(token: Token): HTMLElement {
+		switch (token.role) {
+			case 'meta': return Chordpro.#createMetaElement(token);
+			case 'measures': return Chordpro.#createMeasuresElement(token);
+		}
+	}
+	static #createContentElement(tokens: Token[]): HTMLElement {
 		const element = document.createElement('div');
-		element.classList.add('mono');
+		element.classList.add('content');
 		element.setAttribute('id', Chordpro.#CONTENT_CONTAINER_ID);
 
-		for (const e of Chordpro.#chordproTokens) {
-			switch (e.role) {
-				case 'meta': Chordpro.#appendMeta(e, element); break;
-				case 'measures': Chordpro.#appendMeasures(e, element); break;
-			}
+		for (const e of tokens) {
+			const content = Chordpro.#createTokenElement(e);
+			element.appendChild(content);
 		}
 
-		Chordpro.#sourceRoot.parentElement!.before(element);
+		return element;
 	}
 
-	static #appendScrollConfig(parent: Element) {
+	static #createScrollConfig(): HTMLElement {
 		const containerElement = document.createElement('form');
 		const labelElement = document.createElement('label');
 		const inputElement = document.createElement('input');
@@ -221,16 +240,47 @@ class Chordpro {
 			AutoScroller.stop();
 		});
 		
-		parent.appendChild(containerElement);
+		return containerElement;
 	}
-	static #appendSidemenu() {
+	static #createFontsizeConfig(): HTMLElement {
+		const containerElement = document.createElement('form');
+		const labelElement = document.createElement('label');
+		const inputElement = document.createElement('input');
+		const submitElement = document.createElement('button');
+
+		containerElement.setAttribute('name', 'font_size_form');
+		labelElement.textContent = 'font-size: ';
+		labelElement.setAttribute('for', 'font_size');
+		inputElement.setAttribute('name', 'font_size');
+		inputElement.setAttribute('id', 'font_size');
+		inputElement.setAttribute('type', 'text');
+		inputElement.setAttribute('placeholder', '15px');
+		submitElement.textContent = 'Apply';
+		submitElement.setAttribute('type', 'submit');
+
+		containerElement.appendChild(labelElement);
+		containerElement.appendChild(inputElement);
+		containerElement.appendChild(submitElement);
+
+		containerElement.addEventListener('submit', (event) => {
+			event.preventDefault();
+			const formdata = new FormData(containerElement);
+			const font_size = formdata.get('font_size');
+			if (typeof font_size !== 'string') {throw new Error('Invalid input value')}
+			Chordpro.#contentElement.style.fontSize = font_size;
+		});
+		
+		return containerElement;
+	}
+	static #createSidemenuElement(): HTMLElement {
 		const element = document.createElement('div');
 		element.classList.add('sidemenu-container');
 		element.setAttribute('id', Chordpro.#SIDEMENU_CONTAINER_ID);
 
-		Chordpro.#appendScrollConfig(element);
+		element.appendChild(Chordpro.#createScrollConfig());
+		element.appendChild(Chordpro.#createFontsizeConfig());
 
-		Chordpro.#sourceRoot.parentElement!.before(element);
+		return element;
 	}
 
 	static apply() {
@@ -242,10 +292,17 @@ class Chordpro {
 
 		const hasChordpro = Chordpro.#readElement();
 		if (!hasChordpro) return;
+
 		Chordpro.#applyStyle();
-		Chordpro.#parseLines();
-		Chordpro.#appendSidemenu();
-		Chordpro.#appendContent();
+		
+		const tokens = Chordpro.#parseLines();
+
+		const sidemenu = Chordpro.#createSidemenuElement();
+		const content = Chordpro.#createContentElement(tokens);
+		Chordpro.#contentElement = content;
+
+		Chordpro.#sourceRoot.parentElement!.before(sidemenu);
+		Chordpro.#sourceRoot.parentElement!.before(content);
 	}
 }
 
